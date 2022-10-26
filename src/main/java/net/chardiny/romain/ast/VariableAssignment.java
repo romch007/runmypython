9f47341afb39c1b8f21919f528e242c1d77a2800
Romain Chardiny <romain.chardiny@gmail.com>
2022-10-26 21:03:08 +0200

    convert some classes to record

--- src/main/java/net/chardiny/romain/ast/VariableAssignment.java
+++ src/main/java/net/chardiny/romain/ast/VariableAssignment.java
@@ -1,24 +1,9 @@
 package net.chardiny.romain.ast;
 
-public class VariableAssignment implements Node {
-    private final VariableName lvalue;
-    private final Expression rvalue;
-
-    public VariableAssignment(VariableName lvalue, Expression rvalue) {
-        this.lvalue = lvalue;
-        this.rvalue = rvalue;
-    }
+public record VariableAssignment(VariableName lvalue, Expression rvalue) implements Node {
 
     @Override
     public void execute() {
         System.out.println("assigning variable :)");
-    }
-
-    public VariableName getLvalue() {
-        return lvalue;
-    }
-
-    public Expression getRvalue() {
-        return rvalue;
     }
 }
--- src/main/java/net/chardiny/romain/ast/VariableName.java
+++ src/main/java/net/chardiny/romain/ast/VariableName.java
@@ -1,13 +1,4 @@
 package net.chardiny.romain.ast;
 
-public class VariableName implements Expression {
-    private final String name;
-
-    public VariableName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
+public record VariableName(String name) implements Expression {
 }
